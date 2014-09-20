4fb77fa7cd76a67476ce7ed1cd7966ffd7712ddd
Quentin Young <unboundedcurve@gmail.com>
2014-09-20 02:59:28 -0400

    fixed formatting

--- screenocr.py
+++ screenocr.py
@@ -4,17 +4,17 @@ import pytesser
 import gtk.gdk
 
 def screenshot():
- w = gtk.gdk.get_default_root_window()
- sz = w.get_size()
- pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
- pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
- if (pb != None):
-     pb.save("screenshot.png","png")
- else:
-     print "Unable to get the screenshot."
+   w = gtk.gdk.get_default_root_window()
+   sz = w.get_size()
+   pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, sz[0], sz[1])
+   pb = pb.get_from_drawable(w,w.get_colormap(), 0, 0, 0, 0, sz[0], sz[1])
+   if (pb != None):
+      pb.save("screenshot.png","png")
+   else:
+      print "Unable to get the screenshot."
 
-if __name__ == "__main__":
- screenshot()
- print "working..."
- txt = pytesser.image_to_string("screenshot.png")
- print txt
+   if __name__ == "__main__":
+      screenshot()
+      print "working..."
+      txt = pytesser.image_to_string("screenshot.png")
+      print txt
